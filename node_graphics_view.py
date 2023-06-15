c4f79f7b49eb781a323cc33273658c25f5850fdd
nikPipe <123520318+nikPipe@users.noreply.github.com>
2023-06-15 12:57:59 -0700

    Three

--- node_graphics_view.py
+++ node_graphics_view.py
@@ -12,6 +12,12 @@ class QDMGraphicsView(QGraphicsView):
         self.initUI()
 
         self.setScene(self.grScene)
+        self.zoomInFactor = 1.25
+        self.zoomClamp = True
+        self.zoom = 10
+        self.zoomStep = 1
+        self.zoomRange = [0, 10]
+
 
     def initUI(self):
         '''
@@ -22,3 +28,129 @@ class QDMGraphicsView(QGraphicsView):
         self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+
+
+
+    def mousePressEvent(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonPress(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonPress(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonPress(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonRelease(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonRelease(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonRelease(event)
+        else:
+            super().mouseReleaseEvent(event)
+
+
+    def middleMouseButtonPress(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        fake_event = QMouseEvent(event.MouseButtonRelease, event.localPos(), event.screenPos(),
+                                 Qt.LeftButton, Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(fake_event)
+
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        fake_event = QMouseEvent(event.MouseButtonPress, event.localPos(), event.screenPos(),
+                                    Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers())
+        super().mousePressEvent(fake_event)
+
+    def leftMouseButtonPress(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        print('Left mouse button pressed')
+
+    def rightMouseButtonPress(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        print('Right mouse button pressed')
+
+    def middleMouseButtonRelease(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        fake_event = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                    Qt.LeftButton, event.buttons() & ~Qt.LeftButton, event.modifiers())
+        super().mouseReleaseEvent(fake_event)
+        self.setDragMode(QGraphicsView.NoDrag)
+
+    def leftMouseButtonRelease(self, event):
+
+        '''
+
+        :param event:
+        :return:
+        '''
+        print('Left mouse button released')
+
+    def rightMouseButtonRelease(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        print('Right mouse button released')
+
+    def wheelEvent(self, event):
+        '''
+
+        :param event:
+        :return:
+        '''
+        #CALCULATE ZOOM FACTOR
+        zoomOutFactor = 1 / self.zoomInFactor
+
+        #CALCULATE ZOOM
+        if event.angleDelta().y() > 0:
+            zoomFactor = self.zoomInFactor
+            self.zoom += self.zoomStep
+        else:
+            zoomFactor = zoomOutFactor
+            self.zoom -= self.zoomStep
+
+        clamped = False
+        if self.zoom < self.zoomRange[0]:
+            self.zoom, clamped = self.zoomRange[0], True
+        if self.zoom > self.zoomRange[1]:
+            self.zoom, clamped = self.zoomRange[1], True
+
+        #SET NEW SCALE
+        if not clamped or self.zoomClamp is False:
+            self.scale(zoomFactor, zoomFactor)
+
+
+
+
+
+
